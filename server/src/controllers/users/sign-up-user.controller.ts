be1bdc0efcd39f41cd7fe6390f9d1e36cbf89c6b
Akiru1104 <zulaa.1104@gmail.com>
2026-01-31 17:30:43 +0800

    1/31

--- server/src/controllers/users/sign-up-user.controller.ts
+++ server/src/controllers/users/sign-up-user.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { UserModel } from "../../models/user.model";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
 import { verifyUserEmail } from "../../utils/mail-utils";
 
 export const signUpUser = async (req: Request, res: Response) => {
@@ -8,6 +9,8 @@ export const signUpUser = async (req: Request, res: Response) => {
     const { password, email } = req.body;
 
     const hashedPassword = await bcrypt.hash(password, 10);
+
+    const token = jwt.sign({_id:""}, "hello", {exiresIn: "2h"}); 
 
     const sign = await UserModel.create({
       email,
