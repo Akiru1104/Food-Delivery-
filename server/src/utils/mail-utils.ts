a20e5bfe2a89458a65613e98a2aed7a02a6f4026
Akiru1104 <zulaa.1104@gmail.com>
2026-02-06 17:52:37 +0800

    changes

--- server/src/utils/mail-utils.ts
+++ server/src/utils/mail-utils.ts
@@ -1,22 +1,14 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
 import { configDotenv } from "dotenv";
+import { Resend } from "resend";
 
 configDotenv();
 
-const { AUTH_MAIL, AUTH_PASS } = process.env;
-
-const transport = nodemailer.createTransport({
-  service: "gmail",
-  port: 587,
-  auth: {
-    user: AUTH_MAIL,
-    pass: AUTH_PASS,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const verifyUserEmail = async (receiver: string, verifyLink: string) => {
-  await transport.sendMail({
-    from: `"Food Delivery " ${AUTH_MAIL}`,
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
     to: receiver,
     subject: "Verify user",
     html: `<div style="width: 300px; height: 250px; border-radius: 8px; background-color: aquamarine;">
