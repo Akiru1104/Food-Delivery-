9da4409801772394f58b7ac8776444c2cf5fdb1b
Akiru1104 <zulaa.1104@gmail.com>
2026-02-06 12:08:51 +0800

    as

--- server/src/utils/mail-utils.ts
+++ server/src/utils/mail-utils.ts
@@ -7,6 +7,7 @@ const { AUTH_EMAIL, AUTH_PASS } = process.env;
 
 const transport = nodemailer.createTransport({
   service: "gmail",
+  port: 587,
   auth: {
     user: AUTH_EMAIL,
     pass: AUTH_PASS,
